96eaa7dabf35764a48626f274f5101db934e2903
Сергей Осипов <mrfix1033@mail.ru>
2024-10-18 23:25:19 +0300

    abandoning the structure of the code

--- app/src/main/java/ru/mrfix1033/checkboxesandsnackbar/MainActivity.kt
+++ app/src/main/java/ru/mrfix1033/checkboxesandsnackbar/MainActivity.kt
@@ -1,19 +1,42 @@
 package ru.mrfix1033.checkboxesandsnackbar
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import ru.mrfix1033.checkboxesandsnackbar.managers.ButtonClickManager
-import ru.mrfix1033.checkboxesandsnackbar.managers.ViewsManager
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var viewsManager: ViewsManager private set
+    lateinit var etEnterText: EditText
+    lateinit var tvLabel: TextView
+    lateinit var btnSaveData: Button
+    lateinit var btnDeleteData: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewsManager = ViewsManager(this)
-        ButtonClickManager(this)
+        etEnterText = findViewById(R.id.etEnterText)
+        tvLabel = findViewById(R.id.tvLabel)
+        btnSaveData = findViewById(R.id.btnSaveData)
+        btnDeleteData = findViewById(R.id.btnDeleteData)
+
+        findViewById<Button>(R.id.btnSaveData).setOnClickListener {
+            tvLabel.text = etEnterText.text
+        }
+
+        findViewById<Button>(R.id.btnDeleteData).setOnClickListener {
+            Snackbar.make(it, R.string.delete_confirmation, Snackbar.LENGTH_LONG)
+                .setAction(R.string.confirmation_label) { itSnackBar ->
+                    tvLabel.text = ""
+                    Snackbar.make(
+                        itSnackBar,
+                        R.string.you_confirmed_delete,
+                        Snackbar.LENGTH_SHORT
+                    ).show()
+                }.show()
+        }
     }
 }
